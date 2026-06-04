b3c2be62c0ad5c3970c73a82eacbd595eecc9f30
Erkut Elik <erkutelik@gmail.com>
2026-06-04 21:48:50 +0300

    search işlemi eklendi

--- BookInfo/views.py
+++ BookInfo/views.py
@@ -4,7 +4,7 @@ from .serializer import KategoriSerializer,KitapInfoSerializer
 from rest_framework.response import Response
 from .models import BookCategori,BookInfo
 from rest_framework import status
-
+from django.db.models import Q
 # Create your views here.
 
 @api_view(['GET'])
@@ -54,12 +54,17 @@ from rest_framework.exceptions import NotFound
 @api_view(['DELETE'])
 def delete_categori(request, slug):
     try:
-        book_categori = BookCategori.objects.get(slug=slug)
+        obj = BookCategori.objects.get(slug=slug)
+        obj.delete()
+        return Response(
+            {'message': 'Kategori silindi'},
+            status=status.HTTP_204_NO_CONTENT
+        )
     except BookCategori.DoesNotExist:
-        raise NotFound({'status':'Kategori bulunamadı'})
-    book_categori.delete()
-
-    return Response({'message': 'Kategori silindi'})
+        return Response(
+            {'error': 'Kategori bulunamadı'},
+            status=status.HTTP_404_NOT_FOUND
+        )
 
 @api_view(['PATCH'])
 def patch_categori(request, slug):
@@ -119,14 +124,21 @@ def kitap_ekle(request):
 
 
 @api_view(['DELETE'])
-def kitap_sil(requset,slug):
+def kitap_sil(request, slug):
     try:
-        book_categori=BookInfo.objects.get(book_slug=slug)
-    except BookInfo.DoesNotExist:
-        raise NotFound({'status':'Kitap silindi'}) 
-    book_categori.delete()
+        obj = BookInfo.objects.get(book_slug=slug)
+        obj.delete()
+        return Response(
+            {'message': 'Kitap silindi'},
+            status=status.HTTP_200_OK
+        )
+    except BookInfo.DoesNotExist as e:
+        return Response(
+            {'error': f'Kitap bulunamadı{e}'},
+            status=status.HTTP_404_NOT_FOUND
+        )
 
-    return Response({'message':'Kitap silinirken hata meydana geldi'})
+
 
 @api_view(['GET'])
 def get_info_book(request,slug):
@@ -138,3 +150,46 @@ def get_info_book(request,slug):
     except BookInfo.DoesNotExist:
         return Response({"message":"Kategori Bulunamadı"},
                         status=404)
+    
+
+@api_view(['PATCH'])
+def get_guncelleme(request,slug):
+    try:
+        instance=BookInfo.objects.get(book_slug=slug)
+        serializer=KitapInfoSerializer(instance,data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status':'Güncelleme işlemi tamamlandı',
+                            'data':serializer.data},
+                            status=200)
+    
+    except BookCategori.DoesNotExist:
+        return Response({'error':'Silinmes sırasında bir hata meydana geldi'},status=404)
+
+
+@api_view(['GET'])
+def get_all(request):
+    menu=BookInfo.objects.all()
+    try:
+        serializer = KitapInfoSerializer(menu, many=True)
+        return Response({'status':'Tüm kitap bilgileri','data':serializer.data})
+    except:
+        return Response({'status':'Kitap bilgileri getirilirke hata meydana geldi'},status=status.HTTP_204_NO_CONTENT)
+
+
+
+@api_view(['GET'])
+def get_search(request, name):
+    kitaplar = BookInfo.objects.filter(
+        Q(book_name__icontains=name) |
+        Q(book_slug__icontains=name)
+    )
+
+    if kitaplar.exists():
+        serializer = KitapInfoSerializer(kitaplar, many=True)
+        return Response({'status':'başarıyla bulundu',
+                        'data':serializer.data},
+                        status=status.HTTP_302_FOUND)
+    
+    else:
+        return Response({'erorr':'Kitap bulunamadı'},status=status.HTTP_404_NOT_FOUND)
---
+++ Tests/test_book_info.py
@@ -0,0 +1,135 @@
+import pytest
+import requests
+
+
+class TestBookInfo:
+    URL = "http://127.0.0.1:8000/book-categori"
+    GET_ALL = f"{URL}/book-all/"
+    GET_POST = f"{URL}/book-add/"
+    GET_DELETE = f"{URL}/book-delete/"
+    GET_PATCH = f"{URL}/book-update/"
+    GET_FIRST = f"{URL}/book-get/"
+    GET_SEARCH=f"{URL}/search/"
+
+    @pytest.fixture
+    def create_book_info(self):
+        "Yeni kitap oluşturma fixture"
+        import random
+        def _create(book_name="test123",barcode=random.randint(1000,9999),price=12,write="Kitap yazarı test",kategori=1):
+            data = {
+                "book_name": book_name,
+                "barcode": f'1231{barcode}',
+                "price": price,
+                "writer": write,
+                "kategori": kategori
+            }
+            try:
+                response = requests.post(url=TestBookInfo.GET_POST,json=data)
+                return response
+
+            except Exception as e:
+                return {'erorr':f'Hata meydana geldi{e}'}
+
+        return _create
+
+    @pytest.fixture
+    def book_info_delete(self):
+        'kitap silme fixture'
+        def _delete(slug):
+            url_=f'{TestBookInfo.GET_DELETE}{slug}/'
+            response=requests.delete(url=url_)
+            return response
+
+        return _delete
+
+    def test_kitap_ekleme(self, create_book_info,book_info_delete):
+        "Yeni bir kitap ekleme"
+        VALUE='erkut-elik-deneme'
+        add_response=create_book_info(VALUE)
+        delete_response=book_info_delete(VALUE)
+
+    def test_kitap_silme(self,create_book_info,book_info_delete):
+        'yeni bir kitap ekle ve kitabı sil'
+        VALUE='Erkut Elik Yazı'
+        ekle_=create_book_info(VALUE)
+        assert ekle_.status_code==201
+        assert ekle_.json()['status']=='Başarıyla yeni kitap eklendi'
+        slug=ekle_.json()['data']['book_slug']
+
+        sil=book_info_delete(slug)
+        assert sil.status_code==200
+        assert sil.json()['message']=='Kitap silindi'
+
+
+    def test_ayni_isimde_kitap_ekleme(self,create_book_info,book_info_delete):
+        "Yeni bir kitap ekleme"
+        from time import sleep
+        VALUE='eererewew'
+        deger1=create_book_info(VALUE)
+        assert deger1.status_code==201
+        assert deger1.json()['status']=='Başarıyla yeni kitap eklendi'
+        sleep(2)
+        deger=create_book_info(VALUE)
+        assert deger.status_code==400
+        assert deger.json()['status']=='hata meydana geldi'
+        sleep(2)
+        silme=book_info_delete(VALUE)
+
+    def test_kitap_düzenleme(self,create_book_info,book_info_delete):
+        "bir kitap ekle ve üzerinde güncelleme yap"
+        from time import sleep
+        new_book=create_book_info('isim')
+        assert new_book.status_code==201,new_book.text
+        assert new_book.json()['status']=='Başarıyla yeni kitap eklendi',new_book.text
+
+
+        guncel_veriler={
+            'book_name':'isim_12321'
+        }
+
+
+        eski_slug=new_book.json()['data']['book_slug']
+        response=requests.patch(url=f'{self.GET_PATCH}{eski_slug}/',json=guncel_veriler)
+
+        assert response.status_code==200
+        assert response.json()['status']=='Güncelleme işlemi tamamlandı'
+
+        response=response.json()['data']['book_slug']
+
+        sil_=book_info_delete(response)
+        assert sil_.status_code==200
+        assert sil_.json()['message']=='Kitap silindi'
+
+
+
+    def test_kitap_search(self,create_book_info,book_info_delete):
+        yeni_eklenen_kitap=create_book_info('bu deneme yazısıdır')
+        ara=requests.get(url=f'{self.GET_SEARCH}{yeni_eklenen_kitap.json()['data']['book_slug']}',)
+        new_add_book=yeni_eklenen_kitap.json()['data']
+        search_value=ara.json()['data'][0]
+
+        assert search_value['book_slug']==new_add_book['book_slug'],'book_slug değerinde hata meydana geldi'
+        assert search_value['book_name']==new_add_book['book_name'],'book_name değerinde hata meydana geldi'
+        assert search_value['barcode']==new_add_book['barcode'],'barcode değerinde hata meydana geldi'
+        assert search_value['price']==new_add_book['price'],'price değerinde hata meydana geldi'
+        assert search_value['writer']==new_add_book['writer'],'writer değerinde hata meydana geldi'
+        assert search_value['kategori']==new_add_book['kategori'],'kategori değerinde hata meydana geldi'
+
+
+
+        book_info_delete(new_add_book['book_slug'])
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
