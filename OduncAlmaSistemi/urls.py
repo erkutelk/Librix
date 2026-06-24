b24723ef313bfc66a58f8a3ffa92adb2d1ff96e5
Erkut Elik <erkutelik@gmail.com>
2026-06-24 03:30:48 +0300

    Kullanıcıların aldıkları kitaplara göre ayırma işlemi yapıldı

--- OduncAlmaSistemi/urls.py
+++ OduncAlmaSistemi/urls.py
@@ -2,7 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('odunc-all/',views.odunc_alma_get),
+    path('odunc-all/',views.odunc_alma_get_all),
+    path('users/<int:user_id>/books/',views.kullanicilarin_odunc_aldigi_kitaplar),
     path('admins/odunc-get/<int:id>/',views.odunc_first_id),
     path('admins/odunc-update/<int:id>/',views.odunc_alma_guncelleme),
     path('admins/odunc-create/',views.odunc_create)
--- OduncAlmaSistemi/views.py
+++ OduncAlmaSistemi/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import OduncAlmaSistemi
+from .models import OduncAlmaSistemi,UserInfo
 from .serializer import OduncAlmaSistemiSeriazlier_list,OduncAlmaSistemiCreateSerializer
 from User.permissions import IsAdmin
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.pagination import PageNumberPagination
+from django.shortcuts import get_object_or_404
 
 @api_view(['GET'])
 @permission_classes([IsAdmin])
-def odunc_alma_get(request):
+def odunc_alma_get_all(request):
     models_odunc=OduncAlmaSistemi.objects.all()
 
 
@@ -19,6 +20,16 @@ def odunc_alma_get(request):
     
     deger=OduncAlmaSistemiSeriazlier_list(result_page,many=True)
     return paginator.get_paginated_response(deger.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdmin])
+def kullanicilarin_odunc_aldigi_kitaplar(request,user_id):
+    user=get_object_or_404(UserInfo,id=user_id)
+    oduncler=OduncAlmaSistemi.objects.filter(user=user)
+    serializer=OduncAlmaSistemiSeriazlier_list(oduncler,many=True)
+    return Response({"user":{"id":user_id,
+                             "username":user.username},
+                             "books":serializer.data})
 
 @api_view(['GET'])
 @permission_classes([IsAdmin])
